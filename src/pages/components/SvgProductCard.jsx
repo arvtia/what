b0f002e540d516a9e87d3154cfd2c984d45eff0c
Arvtia <arvazanakuzhy@gmail.com>
2025-12-24 19:00:03 +0530

    svg - rotate -> 360

--- src/pages/components/SvgProductCard.jsx
+++ src/pages/components/SvgProductCard.jsx
@@ -15,25 +15,27 @@ const InvertedCorner = ({ className } ) => (
 
 export default function SVGProductCard() {
   return (
-    <div className="relative w-80 h-96 bg-neutral-200 rounded-[20px] overflow-hidden group">
-      {/* Background Image */}
-      <img 
-        src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1000" 
-        className="w-full h-full object-cover"
-      />
+      <div className="p-3 bg-white rounded-3xl shadow-sm">
+         <div className="relative w-80 h-96 bg-neutral-200 rounded-[20px] overflow-hidden group">
+         {/* Background Image */}
+         <img 
+         src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=1000" 
+         className="w-full h-full object-cover"
+         />
 
-      {/* The Bottom Tab */}
-      <div className="absolute bottom-0 right-0 bg-white pt-2 pl-2 rounded-tl-[20px]">
-        <button className="bg-black text-white px-8 py-1 mb-3 mr-3 rounded-xl font-bold hover:scale-105 transition-transform">
-          Buy Now
-        </button>
+         {/* The Bottom Tab */}
+         <div className="absolute bottom-0 right-0 bg-white pt-2 pl-2 rounded-tl-[20px]">
+         <button className="bg-white text-white px-8 py-1  mr-3 rounded-xl font-bold hover:scale-105 transition-transform">
+            Buy Now
+         </button>
 
-        {/* The SVG Inverted Curves */}
-        {/* Top Curve */}
-        <InvertedCorner className="absolute -top-[24px] right-0 w-6 h-6 text-white" />
-        
-        {/* Left Curve */}
-        <InvertedCorner className="absolute bottom-0 -left-[24px] w-6 h-6 text-white rotate-90" />
+         {/* The SVG Inverted Curves */}
+         {/* Top Curve */}
+         <InvertedCorner className="absolute -top-[24px] right-0 w-6 h-6 text-white" />
+         
+         {/* Left Curve */}
+         <InvertedCorner className="absolute bottom-0 -left-[24px] w-6 h-6 text-white rotate-360" />
+         </div>
       </div>
     </div>
   );
