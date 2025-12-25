f62ac6c91d006159566eb128cdb5289450680521
Arvtia <arvazanakuzhy@gmail.com>
2025-12-25 22:31:16 +0530

    photo - adjustment

--- src/pages/NordenTheme/components/Projects/ProjectPhoto.jsx
+++ src/pages/NordenTheme/components/Projects/ProjectPhoto.jsx
@@ -8,7 +8,7 @@ const NordenProjectPhoto = () => {
       {PROJECTS.map((project) => (
          <Link to={`/work/${project.id}`}
             key={project.id}
-            className="w-full relative max-w-xl mb-3 p-5 md:p-20 bg-stripes h-72 items-center flex rounded-md"
+            className="w-full relative max-w-xl mb-3 p-2 sm:p-4 md:p-16 bg-stripes h-auto py-5 md:h-72 items-center flex rounded-md"
          >
             <div className="absolute top-0 left-0 flex items-start">
                {/* The Title Div (Your design) */}
@@ -21,7 +21,7 @@ const NordenProjectPhoto = () => {
                <InvertedCorner className="text-white -scale-x-100 rotate-90" />
             </div>
 
-            <img src={project.image} className="h-auto w-full p-1 mt-4 ring-1 ring-neutral-300 bg-white rounded-lg shadow-[0_8px_30px_rgb(0,0,0,0.12)]" alt={project.title || 'Project image'} />
+            <img src={project.image} className="h-auto w-full p-1 mt-6 ring-1 ring-neutral-300 bg-white rounded-lg shadow-[0_8px_30px_rgb(0,0,0,0.12)]" alt={project.title || 'Project image'} />
          </Link>
       ))}
     </>
